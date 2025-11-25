e1bd1907caebb0b242806b21dfc80fe7de8a3263
sarah@flyingfoxtoolbox.com <yourgithub@email.com>
2025-11-24 21:46:33 -0500

    basic generator works

--- chattWithMe.js
+++ chattWithMe.js
@@ -1,10 +1,13 @@
 //Personalized greeting
 let userName = "Parth and Sarah";
-userName ? console.log(`Hey, ${userName}! Do you have 4-5 hours to spend together on a date in Chattanooga?`) : console.log("Hello! Do you have 4-5 hours to spend together on a date in Chattanooga?");
+userName
+  ? console.log(`Hey, ${userName}! Here's what you'll do tonight:`)
+  : console.log(
+      "Hello! Do you have 4-5 hours to spend together on a date in Chattanooga?"
+    );
 
 // Restaurant randomizer
 let randomRestaurant = Math.floor(Math.random() * 14);
-console.log(randomRestaurant);
 
 switch (randomRestaurant) {
   case 0:
@@ -57,4 +60,107 @@ switch (randomRestaurant) {
     break;
 }
 
-console.log(restaurant);
+// Activity randomizer
+let randomActivity = Math.floor(Math.random() * 14);
+
+switch (randomActivity) {
+  case 0:
+    activity = "play a round of Dominion.";
+    break;
+  case 1:
+    activity = "play What's Wrong With You?";
+    break;
+  case 2:
+    activity = "draw portraits of each other while blind-folded.";
+    break;
+  case 3:
+    activity =
+      "take Augie to Play, Wash, Pint. If they're not open or the weather's bad, take him to pick out a new toy.";
+    break;
+  case 4:
+    activity = "go bowling!";
+    break;
+  case 5:
+    activity = "go play at the arcade in the Choo-Choo.";
+    break;
+  case 6:
+    activity =
+      "ride bikes unless the weather's bad; in that case, you're going to go ride around in one of those horse carriages. For real.";
+    break;
+  case 7:
+    activity = "lift weights at the YMCA!";
+    break;
+  case 8:
+    activity =
+      "see a movie in a movie theater like people did in the 1950's and such.";
+    break;
+  case 9:
+    activity = "choose each other's outfit for the night.";
+    break;
+  case 10:
+    activity = "go to Synergy or High Point to do some climbing.";
+    break;
+  case 11:
+    activity = "do a puzzle together.";
+    break;
+  case 12:
+    activity = "plan the next murder mystery party you'll host.";
+    break;
+  case 13:
+    activity = "bake cookies together.";
+    break;
+  case 14:
+    activity = "give each other a positive performance review";
+    break;
+}
+
+// Challenge randomizer
+let randomChallenge = Math.floor(Math.random() * 11);
+
+switch (randomChallenge) {
+  case 0:
+    challenge =
+      "ask a stranger who does NOT have a dog if you may pet their dog";
+    break;
+  case 1:
+    challenge = "you may only eat food and drink left-handed";
+    break;
+  case 2:
+    challenge =
+      "the only food and drink you may consume must begin with the letter 'R'";
+    break;
+  case 3:
+    challenge =
+      "Ask a third party in the restaurant to order your food for you. You cannot sway their opinion or offer guidance (except to avoid food allergies)";
+    break;
+  case 4:
+    challenge =
+      "call a friend you haven't spoken to in over a week and tell them that you want to wish them sweet dreams";
+    break;
+  case 5:
+    challenge =
+      "choose something you'd like to get rid off, like an old dog toy or sweater, and then sell it to a stranger for any amount. Take that amount and put it in VTSAX";
+    break;
+  case 6:
+    challenge =
+      "You may only use 1-syllable words during dinner. If one of you slips up, you must both say 'meow'";
+    break;
+  case 7:
+    challenge =
+      "make frequent mention of pot bellied pigs, but pretend that it's a normal thing to mention";
+    break;
+  case 8:
+    challenge = "your shirts must be worn backwards";
+    break;
+  case 9:
+    challenge = "you may not wear shoes that match";
+    break;
+  case 10:
+    challenge = "tell a stranger 3 things you love about your partner";
+    break;
+}
+
+//Mixed Messages Generator
+console.log(
+  `${restaurant} You're also going to ${activity} That sounds great! However, there is a challenge:  ${challenge}. If you fail this challenge, you'll probably like, have to get divorced or something.`
+);
